26a7fa1f9c70cc15183cfb22dc067b14f42354c9
Dmitry Osipovich <dosipovich@hotmail.com>
2022-10-07 16:53:46 +0300

    Первая задача №2 выполнена успешно.

---
+++ zadacha_2.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое целое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе целое число: ");
+int num2 = int.Parse(Console.ReadLine());
+if (num1 > num2)
+{    
+    Console.WriteLine($"Из двух введённых чисел {num1} и {num2} большим является число {num1}.");
+}
+if (num1 < num2)
+{
+    Console.WriteLine($"Из двух введённых чисел {num1} и {num2} большим является число {num2}.");
+}
+if (num1 == num2)
+{
+    Console.WriteLine($"Из двух введённых чисел {num1} и {num2} оба числа равны между собой.");
+}
